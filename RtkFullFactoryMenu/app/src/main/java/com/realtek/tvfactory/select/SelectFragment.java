697dc5690f336344ee70c481bc4c67743444a1ec
zoipuus <813271024@qq.com>
2022-12-13 16:01:53 +0800

    修改CountryLang显示为当前国家和语言

--- RtkFullFactoryMenu/app/src/main/java/com/realtek/tvfactory/select/SelectFragment.java
+++ RtkFullFactoryMenu/app/src/main/java/com/realtek/tvfactory/select/SelectFragment.java
@@ -1,14 +1,11 @@
 package com.realtek.tvfactory.select;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 import com.realtek.tvfactory.BaseFragment;
 import com.realtek.tvfactory.FactoryMenuFragment;
 import com.realtek.tvfactory.R;
-import com.realtek.tvfactory.api.impl.FactoryMainApi;
 import com.realtek.tvfactory.preference.Preference;
 import com.realtek.tvfactory.preference.PreferenceContainer;
 import com.realtek.tvfactory.preference.PreferenceFragment;
@@ -42,23 +39,8 @@ public class SelectFragment extends PreferenceFragment {
             case R.id.page_Project:
                 showPage(ProjectIdFragment.class, R.string.str_select_Project);
                 break;
-            case R.id.country_lang:
-                openDialog(preference.getTitle().toString(), FactoryMainApi.getInstance().getCountryLang());
-                break;
             default:
                 break;
         }
-    }
-
-    private void openDialog(String title, String msg){
-        AlertDialog dialog = new AlertDialog.Builder(getContext(), AlertDialog.THEME_DEVICE_DEFAULT_LIGHT)
-                .setTitle(title).setMessage(msg).setCancelable(true).setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                    }
-                }).setCancelable(false).create();
-        dialog.show();
-        dialog.getButton(DialogInterface.BUTTON_POSITIVE).requestFocus();
-
     }
 }
--- RtkFullFactoryMenu/app/src/main/java/com/realtek/tvfactory/select/SelectLogic.java
+++ RtkFullFactoryMenu/app/src/main/java/com/realtek/tvfactory/select/SelectLogic.java
@@ -1,14 +1,31 @@
 package com.realtek.tvfactory.select;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.RemoteException;
+import android.os.SystemProperties;
+import android.provider.Settings;
+import android.util.Log;
+
+import com.android.internal.app.LocalePicker;
 import com.realtek.tvfactory.R;
 import com.realtek.tvfactory.api.impl.FactoryMainApi;
+import com.realtek.tvfactory.kk.Country;
 import com.realtek.tvfactory.logic.LogicInterface;
 import com.realtek.tvfactory.preference.PreferenceContainer;
 import com.realtek.tvfactory.preference.SeekBarPreference;
 import com.realtek.tvfactory.preference.StatePreference;
 import com.realtek.tvfactory.preference.SumaryPreference;
 
+import java.util.List;
+import java.util.Locale;
+
 public class SelectLogic extends LogicInterface {
+
+    private static final String TAG = SelectLogic.class.getSimpleName();
+    private static final String SYSTEM_COUNTRY = "com.android.tv_current_country";
+
+    private static final int INVALID_COUNTRY_INDEX = -1;
 
     public SelectLogic(PreferenceContainer container) {
         super(container);
@@ -31,7 +48,36 @@ public class SelectLogic extends LogicInterface {
         SumaryPreference boot_animation = (SumaryPreference) mContainer.findPreferenceById(R.id.boot_animation);
         boot_animation.setSumary(mFactoryMainApi.getPanelType());
 
-        ((SumaryPreference) mContainer.findPreferenceById(R.id.country_lang)).setSumary(mContext.getString(R.string.str_click_to_view));
+        String countryName;
+        Country country = Country.getCountry(getCurCountryIndex(mContext));
+        if (country != null) {
+            countryName = mContext.getString(country.getName());
+        } else {
+            countryName = "unknown";
+        }
+
+        Locale currentLocale = null;
+        try {
+            currentLocale = ActivityManager.getService().getConfiguration()
+                    .getLocales().get(0);
+        } catch (RemoteException e) {
+            Log.e(TAG, "RemoteException:" + e);
+        }
+
+        final List<LocalePicker.LocaleInfo> localeInfoList =
+                LocalePicker.getAllAssetLocales(mContainer.getContext(), false);
+        LocalePicker.LocaleInfo currentLocaleInfo = null;
+        for (final LocalePicker.LocaleInfo localeInfo : localeInfoList) {
+            if (localeInfo.getLocale().equals(currentLocale)) {
+                currentLocaleInfo = localeInfo;
+                break;
+            }
+        }
+        if (currentLocaleInfo != null) {
+            ((SumaryPreference) mContainer.findPreferenceById(R.id.country_lang)).setSumary(countryName + " / " + currentLocaleInfo.getLabel());
+        } else {
+            ((SumaryPreference) mContainer.findPreferenceById(R.id.country_lang)).setSumary(countryName + " / " + SystemProperties.get("persist.sys.locale", ""));
+        }
     }
 
     @Override
@@ -47,5 +93,9 @@ public class SelectLogic extends LogicInterface {
     @Override
     public void onPreferenceIndexChange(StatePreference preference, int previous, int current) {
 
+    }
+
+    private int getCurCountryIndex(Context context) {
+        return Settings.Global.getInt(context.getContentResolver(), SYSTEM_COUNTRY, INVALID_COUNTRY_INDEX);
     }
 }
